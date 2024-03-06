e74dad9e77de3fe41935b9d06e9271d0d6474ebb
Carlo Nucera <carlo.nucera@protonmail.com>
2024-03-06 16:10:32 +0100

    016: try to use a different stdenv

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true; # needed for CUDA on Linux
+          cudaSupport = true;
         };
 
         inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; })
@@ -23,21 +24,23 @@
 
       in {
 
-        devShells.minimal = pkgs.mkShell {
-          name = "minimal-dev-shell";
-          buildInputs =
-            [ (pkgs.python311.withPackages (p: [ p.poetry-core ])) ];
-        };
+        devShells.minimal =
+          (pkgs.mkShell.override { stdenv = pkgs.gcc11Stdenv; }) {
+            name = "minimal-dev-shell";
+            buildInputs =
+              [ (pkgs.python311.withPackages (p: [ p.poetry-core ])) ];
+          };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.ninja
-            (mkPoetryEnv {
-              python = pkgs.python311;
-              projectDir = ./.;
-              overrides = import ./overrides.nix { inherit overrides; };
-            })
-          ];
-        };
+        devShells.default =
+          (pkgs.mkShell.override { stdenv = pkgs.gcc11Stdenv; }) {
+            buildInputs = [
+              pkgs.ninja
+              (mkPoetryEnv {
+                python = pkgs.python311;
+                projectDir = ./.;
+                overrides = import ./overrides.nix { inherit overrides; };
+              })
+            ];
+          };
       });
 }
